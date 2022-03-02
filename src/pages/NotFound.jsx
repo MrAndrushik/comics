c71db18300d607bb738b316d8998d966927d331c
mrandrushik <mrandrushik@gmail.com>
2022-03-02 20:58:24 +0300

    change NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import SearchInput from "../components/SearchInput";
-import { objForMiddleSlider } from "../data";
 
 const NotFound = () => {
     return (
